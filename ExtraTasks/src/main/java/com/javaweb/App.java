87ff4a3197e6adab4f75ae1199bff73022f1e387
Chernysh Andrey <itcherry97@gmail.com>
2016-11-03 00:12:46 +0200

    Other extra tasks has been added.

--- ExtraTasks/src/main/java/com/javaweb/App.java
+++ ExtraTasks/src/main/java/com/javaweb/App.java
@@ -15,16 +15,16 @@ public class App {
         System.out.println("Sum of elements : " + solver.sumElements());
 
         //2
-        int [] maxElement = solver.findMax();
+        int[] maxElement = solver.findMax();
         System.out.println("Max element : array[" +
                 maxElement[ArraySolver.ELEMENT_INDEX] + "] = " +
                 maxElement[ArraySolver.ELEMENT_VALUE] + ";");
 
         //3
-        int [] minElement = solver.findMin();
+        int[] minElement = solver.findMin();
         System.out.println("Min element : array[" +
-                maxElement[ArraySolver.ELEMENT_INDEX] + "] = " +
-                maxElement[ArraySolver.ELEMENT_VALUE] + ";");
+                minElement[ArraySolver.ELEMENT_INDEX] + "] = " +
+                minElement[ArraySolver.ELEMENT_VALUE] + ";");
 
         //4
         System.out.println("Average : " + solver.getAverage());
@@ -70,5 +70,75 @@ public class App {
         //12
         System.out.println("First positive element : " +
                 solver.getFirstPositiveElement());
+
+        //13
+        System.out.println("Last negative element : " +
+                solver.getLastNegativeElement());
+
+        //14
+        System.out.println("Get indexes of value 0 : " +
+                Arrays.toString(solver.getIndexesOfValue(0)));
+
+        //15
+        //-----------------------------------------------------
+        // new values of array
+        int[] array2 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        solver.setArray(array2);
+        System.out.println("Reset array, array : " +
+                Arrays.toString(solver.getArray()));
+
+        System.out.println("is sorted asc : " + solver.isSortedAsc(array2));
+
+        //16
+        System.out.println("is sorted desc : " + solver.isSortedDesc(array2));
+
+        //17
+        System.out.println("Your array before shift right 4: " +
+                Arrays.toString(solver.getArray()));
+        solver.shiftRightArray(4);
+        System.out.println("Your array after shift right 4: " +
+                Arrays.toString(solver.getArray()));
+
+        //18 - need to ask
+
+        //19
+        System.out.println("Get count values more than average number : " +
+                solver.getCountValuesMoreAverage());
+
+        //20 - need to ask
+
+        //21
+        int[] secondArray = {6, 10, 7, 9, 11};
+        System.out.println("First array : " +
+                Arrays.toString(solver.getArray()));
+        System.out.println("Second array : " +
+                Arrays.toString(secondArray));
+        System.out.println("get elements from array which are not in" +
+                " second array : " +
+                solver.getElemsArrayNotInSecondArray(secondArray).toString());
+
+        //22
+        System.out.println("Count of elements, which equals with first :" +
+                solver.getCountOfElementsEqualsFirst());
+
+        //23
+        int[] arrayFirst = {3, 4, 5, 9, 10, 100, 200};
+        int[] arraySecond = {0, 1, 2, 5, 11, 23, 45, 101};
+        System.out.println("\t\tMake sorted third array from : ");
+        System.out.println("First array : " + Arrays.toString(arrayFirst));
+        System.out.println("Second array : " + Arrays.toString(arraySecond));
+        System.out.println("Third array : " +
+                solver.makeSortedArrayFromTwo(arrayFirst, arraySecond).toString());
+
+        //24
+        int[] arrayFor24 = {-1,-2,3,4,-3,5,10,-15,-20,-30,-40,10,7};
+        solver.setArray(arrayFor24);
+        System.out.println("Array before swap first and last positive : " +
+                Arrays.toString(arrayFor24));
+        solver.changeFirstPositiveWithLastMirror();
+        System.out.println("Array after swap first and last positive : " +
+                Arrays.toString(solver.getArray()));
+
+
     }
 }
--- ExtraTasks/src/main/java/com/javaweb/ArraySolver.java
+++ ExtraTasks/src/main/java/com/javaweb/ArraySolver.java
@@ -1,10 +1,11 @@
 package com.javaweb;
 
+import java.util.ArrayList;
+
 /**
  * Created by Dron on 02-Nov-16.
  */
 public class ArraySolver {
-    public static final int ARRAY_SIZE = 30;
 
     /**
      * Indexes and size for array, which i will use in
@@ -32,34 +33,30 @@ public class ArraySolver {
         int[] result = new int[RESULT_ARRAY_SIZE];
         result[ELEMENT_INDEX] = 0;
         result[ELEMENT_VALUE] = array[0];
-        boolean flag = false;
         for (int i = 1; i < array.length; i++) {
-            if (array[i] > array[i - 1]) {
+            if (array[i] > result[ELEMENT_VALUE]) {
                 result[ELEMENT_INDEX] = i;
                 result[ELEMENT_VALUE] = array[i];
-                flag = true;
             }
         }
-        return flag ? result : null;
+        return result;
     }
 
     public int[] findMin() {
         int[] result = new int[RESULT_ARRAY_SIZE];
         result[ELEMENT_INDEX] = 0;
         result[ELEMENT_VALUE] = array[0];
-        boolean flag = false;
         for (int i = 1; i < array.length; i++) {
-            if (array[i] < array[i - 1]) {
+            if (array[i] < result[ELEMENT_VALUE]) {
                 result[ELEMENT_INDEX] = i;
                 result[ELEMENT_VALUE] = array[i];
-                flag = true;
             }
         }
-        return flag ? result : null;
+        return result;
     }
 
     public double getAverage() {
-        return array.length != 0 ? (this.sumElements() / array.length) : 0.0;
+        return array.length != 0 ? ((double) this.sumElements() / array.length) : 0.0;
 
     }
 
@@ -134,44 +131,132 @@ public class ArraySolver {
     public int[] getIndexesOfValue(int value) {
         int resultArraySize = 0;
         for (int i = 0; i < array.length; i++) {
-            if(array[i] == value){
+            if (array[i] == value) {
                 resultArraySize++;
             }
         }
-        int [] resultArray = new int[resultArraySize];
+        int[] resultArray = new int[resultArraySize];
         int j = 0;
         for (int i = 0; i < array.length; i++) {
-            if(array[i] == value){
+            if (array[i] == value) {
                 resultArray[j++] = i;
             }
         }
         return resultArray;
     }
 
-    public boolean isSortedAsc(){
-        for (int i = 0; i < array.length-1; i++) {
-            if(array[i] < array[i + 1]){
+    public boolean isSortedAsc(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean isSortedDesc(){
-        for (int i = 0; i < array.length-1; i++) {
-            if(array[i] > array[i + 1]){
+    public boolean isSortedDesc(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
                 return false;
             }
         }
         return true;
     }
-    
-   /* public void shiftRightArray(int count){
-        int temp = array[count-1];
-        for (int i = 0; i < count; i++) {
-            array[count - ]
+
+    public void shiftRightArray(int count) {
+        if (count == array.length) {
+            return;
+        } else if (count > array.length) {
+            count -= array.length;
         }
-    }*/
+        for (int i = 0; i < count; i++) {
+            int temp = array[array.length - 1];
+            for (int j = array.length - 2; j >= 0; j--) {
+                array[j + 1] = array[j];
+            }
+            array[0] = temp;
+        }
+    }
+
+    public int getCountValuesMoreAverage() {
+        int count = 0;
+        double average = getAverage();
+        for (int elem : array) {
+            if (elem > average) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public ArrayList<Integer> getElemsArrayNotInSecondArray(int[] secondArray) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            while ((secondArray[j++] != array[i]) && (j < secondArray.length)) {
+            }
+            if (j >= secondArray.length) {
+                result.add(array[i]);
+            }
+            j = 0;
+        }
+        return result;
+    }
+
+    public int getCountOfElementsEqualsFirst() {
+        return getCountEquals(array[0]);
+    }
+
+    public ArrayList<Integer> makeSortedArrayFromTwo(int[] array1, int[] array2) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (isSortedAsc(array1) && isSortedAsc(array2)) {
+            int j = 0;
+            for (int i = 0; i < array1.length; i++) {
+                while ((j < array2.length) && (array2[j] <= array1[i])) {
+                    result.add(array2[j]);
+                    j++;
+                }
+                result.add(array1[i]);
+            }
+        } else if (isSortedDesc(array1) && isSortedDesc(array2)) {
+            int j = 0;
+            for (int i = 0; i < array1.length; i++) {
+                while ((j < array2.length) && (array2[j] >= array1[i])) {
+                    result.add(array2[j]);
+                    j++;
+                }
+                result.add(array1[i]);
+            }
+        } else {
+            return null;
+        }
+        return result;
+    }
+
+    public void changeFirstPositiveWithLastMirror() {
+        int i = 0;
+        int j = array.length - 1;
+        while ((i <= array.length / 2) &&
+                (j >= array.length / 2 + 1)) {
+            if(array[i] < 0){
+                i++;
+            }
+            if(array[j] < 0){
+                j--;
+            }
+            if(array[i] >= 0 && array[j] >= 0){
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
 
     public int[] getArray() {
         return array;
